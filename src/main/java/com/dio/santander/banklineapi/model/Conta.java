ec80f2a9f964803b4792af551cb95ef1ac76559f
francinaldo <francinaldojuniorxd@gmail.com>
2022-05-08 13:25:08 -0300

    feat: swagger,  models, respository, and service

---
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -0,0 +1,28 @@
+package com.dio.santander.banklineapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+
+@Embeddable
+public class Conta {
+
+    @Column(name = "conta_numero")
+    private long numero;
+    @Column(name = "conta_saldo")
+    private Double saldo;
+
+    public long getNumero() {
+        return numero;
+    }
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+    public Double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+}
